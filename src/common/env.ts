e2ff4a56f0541d1a7c718b3ed1e33c4e528b4a51
rulanugrh <nextarituenak01@gmail.com>
2023-08-16 13:52:45 +0700

    feat: connection to database

---
+++ src/common/env.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const env = {
+    database: {
+        host: process.env.MYSQLDB_HOST,
+        port: process.env.MYSQLDB_PORT,
+        name: process.env.MYSQLDB_NAME,
+        user: process.env.MYSQLDB_USER,
+        pass: process.env.MYSQLDB_PASS,
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import debug from 'debug'
+import { DataSource } from "typeorm"
 
 import { CommonRouting } from './common/routes.config';
 import { TodoRoutes } from './routes/todo.routes';
@@ -11,6 +12,8 @@ import { TodoRepository } from './repository/todo.repository';
 import { TodoService } from './services/todo.service';
 import { Todo } from './entity/todo.entity';
 import { TodoController } from './controller/todo.controller';
+import { Category } from './entity/category.entity';
+import { env } from './common/env';
 
 // creating express server
 const app: express.Application = express();
@@ -19,6 +22,26 @@ const port = 3000;
 const debugLog: debug.IDebugger = debug('app')
 
 const routes: Array<CommonRouting> = [];
+
+const myDataSource = new DataSource({
+    type: "mysql",
+    host: `${env.database.host}`,
+    port: Number(env.database.port),
+    username: env.database.user,
+    password: env.database.pass,
+    database: env.database.name,
+    entities: [Todo, Category],
+})
+
+myDataSource.initialize()
+    .then(() => {
+        console.log("Success init database")
+    })
+    .catch((err) => {
+        throw new Error(`Cant init database because : ${err}`)
+    }) 
+
+
 app.use(express.json())
 app.use(cors)
 
