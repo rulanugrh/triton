3cf20b212f4335d241542100bcd6ca2176dc22e3
rulanugrh <nextarituenak01@gmail.com>
2023-08-16 10:30:43 +0700

    feat: cofiguring routing apps

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,42 @@
 import express from 'express';
 import http from 'http';
 import cors from 'cors';
+import debug from 'debug'
+
 import { CommonRouting } from './common/routes.config';
 import { TodoRoutes } from './routes/todo.routes';
+import expressWinston  from 'express-winston';
+import winston from 'winston';
 
+// creating express server
 const app: express.Application = express();
 const server: http.Server = http.createServer(app);
 const port = 3000;
+const debugLog: debug.IDebugger = debug('app')
 
 const routes: Array<CommonRouting> = [];
 app.use(express.json())
 app.use(cors)
+
+// logger options for logging system
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({all: true})
+    )
+}
+
+app.use(expressWinston.logger(loggerOptions))
 routes.push(new TodoRoutes(app))
 
+// listening port
 const runningMessage = `Server running at port : ${port}`
 server.listen(port, () => {
     routes.forEach(( route: CommonRouting) => {
-        console.log(`${route.getName}`)
+        debugLog(`Routes configuration for ${route.name}`)
     })
 
-    console.info(runningMessage)
+    console.log(runningMessage)
 })
